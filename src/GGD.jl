2ba723c29098291ec1fba5fa0bca44ef72c64d3d
Jordan Smith <46058723+jsmithnh09@users.noreply.github.com>
2022-02-04 02:45:17 -0500

    Re-formatting `GGD.jl` using `Pkg.jl`.

--- src/GGD.jl
+++ src/GGD.jl
@@ -1,57 +1,49 @@
+"""
+Generalized Gaussian Distribution package.
+"""
 module GGD
 
-using Statistics, Distributions, SpecialFunctions, Random
-import SpecialFunctions: gamma
-import Distributions: Gamma, sampler, rand, Bernoulli
+using Distributions, SpecialFunctions, Statistics, Random
+using SpecialFunctions: gamma
+using Distributions: Bernoulli, Gamma, sampler, rand
+import Base: rand
 
+### source files
+include("generalizedgaussian.jl")
+
+### methods
 export
-    ### methods
-#    mle,       # maximum likelihood function of shape β
-    gcmsearch,  # Global Convergence Method Newton-Raphson search
-    params,     # get the distribution parameters (GGD Type)
-    shape,      # get the shape parameter (GGD Type)
-    scale,      # get the scale parameter (GGD Type)
-    location,   # get the value of mu (GGD Type)
-    std,        # get the standard deviation (GGD Type)
-    mean,       # get the location parameter (GGD Type)
-    median,     # median of (GGD Type)
-    mode,       # get the mode of the distribution.
-    var,        # get the variance of the distribution.
-    skewness,   # skewness of the distribution.
-    kurtosis,   # EXCESS KURTOSIS, includes the subtraction of 3.
-    entropy,    # entropy function of the distribution.
-    logpdf,     # log of the PDF function.
-    cdf,        # CDF of GGD curve. Uses Gamma Distribution for lower incomplete gamma use.
-    pdf,        # PDF of GGD curve.
-    rand,       # sample from GGD distribution.
+   GeneralizedGaussian,     # principle distribution type.
+   params,                  # returns shape, location, and scale parameters.
+   shape,                   # shape parameter β.
+   scale,                   # scale parameter α, which is related to variance via the shape parameter and gamma functions.
+   location,                # location parameter μ, the same as the mean/median/mode.
+   std,                     # standard deviation σ.
+   mean,                    # mean of the distribution (equal to μ)
+   median,                  # median of distribution
+   mode,                    # mode of distribution
+   var,                     # variance of distribution, (σ²)
+   skewness,                # skewness of Generalized Gaussian Distribution is zero.
+   kurtosis,                # Excess kurtosis of distribution, (this subtracts 3.)
+   entropy,                 # entropy of distribution
+   logpdf,                  # log probability density
+   cdf,                     # cummulative distribution function
+   pdf,                     # probability density function
+   randggd                  # random sample from the distribution
 
+"""
+A Julia Package for the Generalized Gaussian Distribution (GGD).
 
+API:
+- `d = GGD(params...)` creates the GGD Type, containing the relevant
+    distribution parameters, including mu, sigma, alpha, and beta.
+- `z = rand(rng, d)` will generate a sample from the specified GGD
+    distribution "d".
+- `pdf(d, x)` computes the probability density function at x for GGD
+    distribution "d".
+- `cdf(d, x)` computes the cumulative distribution function at x.
 
-    ### types
-    GeneralizedGaussian # generic type containing params (μ, σ, α, β)
+"""
 
-    ### source files
-
-    # types constructor
-    include("generalizedgaussian.jl")
-
-    
-    """
-    A Julia Package for the Generalized Gaussian Distribution (GGD).
-
-    API:
-    - `d = GGD(params...)` creates the GGD Type, containing the relevant
-        distribution parameters, including mu, sigma, alpha, and beta.
-    - `f = GGDFunction(params...)` creates a GGDFunction type, specific to MLE,
-        GCM, and Moment-based functions and their associated derivatives.
-    - `Z = ggdrnd(n, d)` will generate "n" samples from the specified GGD
-        distribution "d".
-    - `pdf(d, x)` computes the probability density function at x for GGD
-        distribution "d".
-    - `cdf(d, x)` computes the cumulative distribution function at x.
-
-    """
-
-    GGD
 
 end # module
